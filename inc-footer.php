4376e264e580257475ade6db5d510e36398b771e
Nicolas Hoizey <nicolas@hoizey.com>
2009-10-01 11:41:58 +0200

    move the about

--- inc-footer.php
+++ inc-footer.php
@@ -1,14 +1,19 @@
-		<div id="footer">
+    <div id="footer">
       <p style="float: right">
         &copy; 2009 <a href="http://www.gasteroprod.com/">Nicolas HOIZEY</a>
       </p>
-	    <p>
-	      <a href="<?php echo $casimir->base_url; ?>">Home</a> |
+      <p>
+        <a href="<?php echo $casimir->base_url; ?>">Home</a> |
         <a href="<?php echo $casimir->base_url; ?>stats.php">Stats</a> |
-	<a href="http://wiki.github.com/nhoizey/casimir">about</a>
+        <a href="http://cas.im/ir/">About</a>
       </p>
-    </div>    <?php    if (file_exists('user-footer.php')) {      require 'user-footer.php';    }    ?>
-	</div>
+    </div>
+    <?php
+    if (file_exists('user-footer.php')) {
+      require 'user-footer.php';
+    }
+    ?>
+  </div>
 </body>
 
 </html>
