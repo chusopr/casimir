02794c6839682ce647e7952129144e47b7e87ca2
Nicolas Hoizey <nicolas@hoizey.com>
2009-10-01 11:29:16 +0200

    Remove ads from casimir code, but add support for a user-footer.php

--- inc-footer.php
+++ inc-footer.php
@@ -7,18 +7,7 @@
         <a href="<?php echo $casimir->base_url; ?>stats.php">Stats</a> |
 	<a href="http://wiki.github.com/nhoizey/casimir">about</a>
       </p>
-    </div>
-		<script type="text/javascript"><!--
-		google_ad_client = "pub-1634609640971475";
-		/* 728x90, date de création 15/09/09 */
-		google_ad_slot = "7381028335";
-		google_ad_width = 728;
-		google_ad_height = 90;
-		//-->
-		</script>
-		<script type="text/javascript"
-		src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-		</script>
+    </div>    <?php    if (file_exists('user-footer.php')) {      require 'user-footer.php';    }    ?>
 	</div>
 </body>
 
