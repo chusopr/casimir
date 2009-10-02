2d4bb8a0c99bbf2d90eb424fb44da4e1dc24e692
Nicolas Hoizey <nicolas@hoizey.com>
2009-10-02 10:59:28 +0200

    move the bookmarklet to a tools page, design improvements

---
+++ tools.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'user/casimir-conf.php';
+require_once 'inc/Casimir.php';
+
+$casimir = new Casimir();
+
+require_once 'inc/header.php';
+?>
+<h3>Tools</h3>
+<h4>Bookmarklet</h4>
+<div id="bookmarklet">Drag this bookmarklet into your toolbar: <?php $casimir->showBookmarklet(); ?></div>
+<?php
+require_once 'inc/footer.php';
+?>
