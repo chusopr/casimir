b212555b022d35895d7ade1527a842d094f4dafe
Nicolas Hoizey <nicolas@hoizey.com>
2010-01-05 14:19:15 +0100

    Casimir -> Cas.im/ir

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-	<title>Casimir - Yet Another URL Shortener</title>
+	<title>Cas.im/ir - Yet Another URL Shortener</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="screen.css" />
   <?php
   if (file_exists('user/screen.css')) {
@@ -15,6 +15,6 @@
 
 <body onload="document.getElementById('long').focus();">
 	<div id="main">
-		<h1><a href="<?php echo $casimir->base_url; ?>">Casimir</a></h1>
+		<h1><a href="<?php echo $casimir->base_url; ?>">Cas.im/ir</a></h1>
 		<h2>Yet Another URL Shortener</h2>
 		
