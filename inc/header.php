38e92112b7603efda19be82aac1d085d1e02eeba
neofutur <git-commits@ww7.be>
2010-10-16 03:27:56 +0200

    add checkbox to get page title, and some details to have a better XHTML validation (http://validator.w3.org)

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 	<title>gw.gd - Shortening you !</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<link rel="stylesheet" type="text/css" media="screen" href="screen.css" />
   <?php
   if (file_exists('user/screen.css')) {
