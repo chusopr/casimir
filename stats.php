5480965e04d0ecae03ab3038b007f4c85f884023
Jesús Pérez Rey (Chuso) <git@chuso.net>
2023-12-29 19:09:40 +0100

    Return the correct HTTP status codes and improve error handling.
    Also, don't return SQL error details to the user.

--- stats.php
+++ stats.php
@@ -4,6 +4,11 @@ require_once 'inc/Casimir.php';
 
 $casimir = new Casimir();
 
+$mostused_day = $casimir->getMostUsedLastDays(1, 5);
+$mostused_week = $casimir->getMostUsedLastDays(7, 5);
+$mostused_month = $casimir->getMostUsedLastDays(30, 5);
+$mostused_ever = $casimir->getMostUsedSinceDate();
+
 require_once 'inc/header.php';
 ?>
 <h3><?php echo _("Most followed short URLs"); ?></h3>
@@ -17,29 +22,28 @@ require_once 'inc/header.php';
   <div id="lastday">
     <h4><?php echo _("Last day"); ?></h4>
     <?php
-    echo $casimir->getMostUsedLastDays(1, 5);
+    echo $mostused_day;
     ?>
   </div>
   <div id="lastweek">
     <h4><?php echo _("Last week"); ?></h4>
     <?php
-    echo $casimir->getMostUsedLastDays(7, 5);
+    echo $mostused_week;
     ?>
   </div>
   <div id="lastmonth">
     <h4><?php echo _("Last month"); ?></h4>
     <?php
-    echo $casimir->getMostUsedLastDays(30, 5);
+    echo $mostused_month;
     ?>
   </div>
   <div id="ever">
     <h4><?php echo _("Ever"); ?></h4>
     <?php
-    echo $casimir->getMostUsedSinceDate();
+    echo $mostused_ever;
     ?>
   </div>
 </div>
 
 <?php
 require_once 'inc/footer.php';
-?>
